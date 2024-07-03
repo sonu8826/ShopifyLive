c3ec3483d850bec7065fea97fc9a7acd7b4dd075
Sonu Thakur <sonu88ggn@gmail.com>
2024-07-03 16:09:12 +0530

    changes

--- public/ChatApp/app.js
+++
@@ -1,9 +0,0 @@
-var addMsg = (msg, type) => {
-    var divTag = $("<div/>").text(msg);
-    if (type == 'send') {
-        divTag.addClass("sendMsg");
-    } else {
-        divTag.addClass("receiveMsg");
-    }
-    $(".msgContainer").append(divTag);
-}
